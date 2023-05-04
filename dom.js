c4f48df1a2d69b034f9b1f3997043d4f1173dec7
prateekshajha <prateekshajha2000>
2023-05-05 00:24:01 +0530

    4th commit

--- dom.js
+++ dom.js
@@ -1,43 +1,55 @@
 
 
-console.log("Hello !")
-console.dir(document);
-console.log(document.domain)
-console.log(document.doctype)
-console.log(document.head)
-console.log(document.body)
-console.log(document.all)
-console.log(document.images)
+// console.log("Hello !")
+// console.dir(document);
+// console.log(document.domain)
+// console.log(document.doctype)
+// console.log(document.head)
+// console.log(document.body)
+// console.log(document.all)
+// console.log(document.images)
 
-var title= document.getElementById("header-title");
+// var title= document.getElementById("header-title");
 
-title.textContent="New"
-console.log(title)
+// title.textContent="New"
+// console.log(title)
 
-title.innerText="Item Lister Updated"
-console.log(title)
+// title.innerText="Item Lister Updated"
+// console.log(title)
 
 
-var header=document.getElementById("main-header");
-header.style.border="solid 3px black";
+// var header=document.getElementById("main-header");
+// header.style.border="solid 3px black";
 
-var add_item=document.getElementsByClassName("title")[0];
+// var add_item=document.getElementsByClassName("title")[0];
 
-console.log(add_item)
-add_item.style.color="green";
-add_item.style.fontWeight = 700;
+// console.log(add_item)
+// add_item.style.color="green";
+// add_item.style.fontWeight = 700;
 
-var list_items=document.getElementsByClassName("list-group-item");
+// var list_items=document.getElementsByClassName("list-group-item");
 
-console.log(list_items);
+// console.log(list_items);
 
-list_items[2].style.backgroundColor="yellow";
+// list_items[2].style.backgroundColor="yellow";
 
-for(let i=0;i<list_items.length;i++){
-    list_items[i].style.fontWeight="bold";
-}
+// for(let i=0;i<list_items.length;i++){
+//     list_items[i].style.fontWeight="bold";
+// }
 
-var list2=document.getElementsByTagName("li")
-for(let i=0;i<list2.length;i++){
-    list2[i].style.color="red";
+// var list2=document.getElementsByTagName("li")
+// for(let i=0;i<list2.length;i++){
+//     list2[i].style.color="red";
+// }
+
+var li=document.querySelectorAll('li');
+var li_odd=document.querySelectorAll('li:nth-child(odd)');
+console.log(li);
+console.log(li_odd);
+
+li[1].style.color="green";
+
+
+for(let i=0;i<li_odd.length;i++){
+    li_odd[i].style.backgroundColor="green";
 }
