3ff5e2b8baf6d3aed2b8826a318f3145f92827ea
prateekshajha <prateekshajha2000>
2023-05-06 15:52:09 +0530

    dom manipulations

--- dom.js
+++ dom.js
@@ -1,55 +1,38 @@
+// var itemList = document.querySelector("#items");
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor="#f4f4f4";
+// console.log(itemList.parentNode.parentNode);
+// console.log(itemList.children);
+// itemList.children[1].style.backgroundColor="yellow";
+// console.log(itemList.firstElementChild);
+// console.log(itemList.lastElementChild);
+// console.log(itemList.nextElementSibling);
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.backgroundColor="green";
 
+// var newDiv= document.createElement('div');
+// newDiv.className="hello";
+// newDiv.id="hello1";
+// newDiv.setAttribute('title','Hello Div');
 
-// console.log("Hello !")
-// console.dir(document);
-// console.log(document.domain)
-// console.log(document.doctype)
-// console.log(document.head)
-// console.log(document.body)
-// console.log(document.all)
-// console.log(document.images)
+// var newDivText=document.createTextNode("hello world");
+// newDiv.appendChild(newDivText);
 
-// var title= document.getElementById("header-title");
+// console.log(newDiv)
 
-// title.textContent="New"
-// console.log(title)
+var container = document.querySelector('header container');
+var h1 = document.querySelector('header h1');
 
-// title.innerText="Item Lister Updated"
-// console.log(title)
+console.log(container)
+console.log(h1)
+// container.insertBefore(newDiv,h1);
 
+// var newLI=document.createElement('div')
+// newLI.appendChild(document.createTextNode("helloWorld"));
 
-// var header=document.getElementById("main-header");
-// header.style.border="solid 3px black";
-
-// var add_item=document.getElementsByClassName("title")[0];
-
-// console.log(add_item)
-// add_item.style.color="green";
-// add_item.style.fontWeight = 700;
-
-// var list_items=document.getElementsByClassName("list-group-item");
-
-// console.log(list_items);
-
-// list_items[2].style.backgroundColor="yellow";
-
-// for(let i=0;i<list_items.length;i++){
-//     list_items[i].style.fontWeight="bold";
-// }
-
-// var list2=document.getElementsByTagName("li")
-// for(let i=0;i<list2.length;i++){
-//     list2[i].style.color="red";
-// }
-
-var li=document.querySelectorAll('li');
-var li_odd=document.querySelectorAll('li:nth-child(odd)');
-console.log(li);
-console.log(li_odd);
-
-li[1].style.color="green";
-
-
-for(let i=0;i<li_odd.length;i++){
-    li_odd[i].style.backgroundColor="green";
-}
+// var ul=document.querySelector('ul')
+// var listitems=document.querySelector('#items')
+// var firstitem=listitems.firstElementChild
+// console.log(ul)
+// console.log(newLI)
+// ul.insertBefore(newLI,firstitem)
